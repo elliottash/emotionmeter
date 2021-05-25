ee03352216073f8ba728998b890700084716f656
piriyacoco <50993538+piriyacoco@users.noreply.github.com>
2021-05-25 15:29:59 +0200

    Update emotion-twitter.py

--- emotion-twitter.py
+++ emotion-twitter.py
@@ -119,7 +119,7 @@ class EmotionMeter:
         df['hashtags_length'] = hashtags_length
         return df
 
-    def calculate_all(self, df):
+    def calculate_all(self):
         df = load_data(self.data_path)
         df = self.calculate_score(df)
         df = self.calculate_num_token(df)
