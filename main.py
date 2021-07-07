7d211fe25aa040b78bb601af53e22af7474099a6
piriyacoco <>
2021-07-07 20:05:14 +0200

    update the code

--- main.py
+++
@@ -1,7 +0,0 @@
-
-from emotionmeter import EmotionMeter
-
-meter = EmotionMeter(data_path = "data/ExtractedTweets.csv")
-
-print(meter.sample_emotional_tweets(most_emotional = True)) # print sample most emotional tweets
-print(meter.odd_ratio_hashtag_party(party = "Republican")) # print odd ratio per hashtag in Republican's tweets
